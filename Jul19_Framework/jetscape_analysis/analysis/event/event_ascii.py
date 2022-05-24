ff8606876417ab642e00ab94a8ac970c16a273f2
Luis Alcerro <l.alcerro@cern.ch>
2022-05-23 22:36:08 -0500

    add leptons

--- Jul19_Framework/jetscape_analysis/analysis/event/event_ascii.py
+++ Jul19_Framework/jetscape_analysis/analysis/event/event_ascii.py
@@ -19,24 +19,30 @@ class EventAscii(event_base.EventBase):
     # ---------------------------------------------------------------
     # Constructor
     # ---------------------------------------------------------------
-    def __init__(self, event_hadrons=None, **kwargs):
+    def __init__(self, event_hadrons=None, event_leptons=None, **kwargs):
         super(EventAscii, self).__init__(**kwargs)
         
         self.event_hadrons = event_hadrons
+        self.event_leptons = event_leptons
 
     # ---------------------------------------------------------------
-    # Get list of hadrons.
+    # Get list of hadrons and leptons
     # ---------------------------------------------------------------
     def hadrons(self, min_track_pt=0.):
     
-        return self.particles(self.event_hadrons, min_track_pt=min_track_pt)
+        return self.particles(self.event_hadrons, min_track_pt=min_track_pt, key ='had')
+        
+    def leptons(self, min_track_pt=0.):
+    
+        return self.particles(self.event_hadrons, min_track_pt=min_track_pt, key ='lep')  
 
     # ---------------------------------------------------------------
     # Construct list of HepMC particles from event's list of particles
     # ---------------------------------------------------------------
-    def particles(self, event, min_track_pt=0.):
+    def particles(self, event, min_track_pt=0., key = ''):
     
-        particles = []
+        hadrons = []
+        leptons = []
         for particle in event:
             pid = int(particle[1])
             status = int(particle[2])
@@ -49,8 +55,13 @@ class EventAscii(event_base.EventBase):
             particle = pyhepmc_ng.GenParticle(four_vector, pid, status)
         
             pt = particle.momentum.pt()
-            if pid != 12 and pid != 14 and pid != 16: # Remove neutrinos
+            if abs(pid) != 12 and abs(pid) != 14 and abs(pid) != 16 and abs(pid) != 11 and abs(pid) != 13 and abs(pid) != 15: # Remove leptons
                 if pt > min_track_pt:
-                    particles.append(particle)
-                
-        return particles
+                    hadrons.append(particle)
+            else:
+            	if pt > min_track_pt:
+            		leptons.append(particle)
+        if key == 'had':
+        	return hadrons
+        else:
+        	return leptons
